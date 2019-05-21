da91f3261d88d6abad4c9b0a167dab5fe9329db7
bishion <bishion@163.com>
2019-05-21 10:50:26 +0800

    目前看来可以支持多家商店了

--- spider_main.py
+++ spider_main.py
@@ -1,6 +1,7 @@
 from web_site.ali_shop.ali_shop_service import AliShopService
 if __name__ == '__main__':
-
-    result_list = AliShopService.check_jixianci("https://yxrywy8.1688.com/page/offerlist.htm")
+    main_url = "https://shop52798901llv53.1688.com/page/offerlist.htm"
+    # main_url = "https://yxrywy8.1688.com/page/offerlist.htm"
+    result_list = AliShopService.check_jixianci(main_url)
 
     AliShopService.show_result(result_list)
